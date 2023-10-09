9eabaee9fb1adfb4aa4ae446093e021958575b53
sinaloa10 <oscarsinaloa47@gmail.com>
2023-10-09 15:33:26 -0600

    cambios en pagina

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/PROYECTO STELLAR VOYAGUE/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: 'main.js', // Archivo principal de tu aplicación
-        luna: 'luna/codigo.js', // Archivo JavaScript de la carpeta "luna"
-        marte: 'marte/marte.js', // Archivo JavaScript de la carpeta "marte"
-        menu: 'menu/menu.js', // Archivo JavaScript de la carpeta "menu"
-        mercurio: 'mercurio/mercurio.js', // Archivo JavaScript de la carpeta "mercurio"
-        venus: 'venus/venus.js', // Archivo JavaScript de la carpeta "venus"
-      }
-    }
-  }
+  // ...
 });
-
